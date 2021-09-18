edb27e99d559e938e0d9d52c9e78cf66a4bc2bcb
jyywong <wong.jonathan1@gmail.com>
2021-09-18 17:32:19 -0400

    debug true

--- contactService/settings.py
+++ contactService/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1:8001',
                  'https://dry-shore-22168.herokuapp.com']
